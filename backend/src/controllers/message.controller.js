b5a39f08810685efcf2ca5da89b0cf41b92527fc
Sriram VT <ramzprivate25@gmail.com>
2025-05-17 15:53:22 +0530

    catch error updated

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -29,7 +29,8 @@ export const getMessages = async (req , res) => {
         
         res.status(200).json(messages);
     } catch (error) {
-        
+        console.log("Error in getMessages controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
     }
 }
 
